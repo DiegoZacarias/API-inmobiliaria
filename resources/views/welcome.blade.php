725437a0db9f09ca4d36f11232ddbe88f1bdbcac
DiegoZacarias <diegozaca97@gmail.com>
2020-12-05 17:35:19 -0300

    Vista welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,8 +12,8 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
+                background-color: #273c75;
+                color: #dcdde1;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
@@ -49,13 +49,21 @@
             }
 
             .links > a {
-                color: #636b6f;
+                color: #718093;
                 padding: 0 25px;
                 font-size: 13px;
                 font-weight: 600;
                 letter-spacing: .1rem;
                 text-decoration: none;
                 text-transform: uppercase;
+            }
+
+            .links > a:hover {
+              color: #f5f6fa;
+              transition: opacity .2s ease-out;
+              -moz-transition: opacity .2s ease-out;
+              -webkit-transition: opacity .2s ease-out;
+              -o-transition: opacity .2s ease-out;
             }
 
             .m-b-md {
@@ -81,18 +89,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Inmobiliaria API
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">Instalación</a>
+                    <a href="#">Documentación</a>
+                    <a href="#">URLS</a>
                 </div>
             </div>
         </div>
